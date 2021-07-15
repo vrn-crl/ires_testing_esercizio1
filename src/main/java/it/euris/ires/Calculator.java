514b74cf1c7c3c86aa01f4844650e4f6d7dbd19a
vrn-crl <veronica.cirillo96@gmail.com>
2021-07-15 13:32:08 +0200

    calculator test update with multiplication and division

--- src/main/java/it/euris/ires/Calculator.java
+++ src/main/java/it/euris/ires/Calculator.java
@@ -2,12 +2,31 @@ package it.euris.ires;
 
 public class Calculator {
 
+	//ADDITION
 	public int add(int a, int b) {
 		return a + b;
 	}
 
+	//SUBSTRACTION
 	public int sub(int a, int b){
 		return a - b;
 	}
 
+	//MULTIPLICATION
+	public int multiplication(int a, int b){
+		return a*b;
+	}
+
+	//DIVISION WITHOUT DECIMAL
+	public int division(int a, int b) throws Exception{
+		if (b == 0) {
+			throw new Exception("b can't be 0");
+		}
+		return a / b;
+	}
+
+	public static void main(String[] args) throws Exception {
+		Calculator c = new Calculator();
+		System.out.println(c.division(10, 7));
+	}
 }
--- src/test/java/it/euris/ires/CalculatorTest.java
+++ src/test/java/it/euris/ires/CalculatorTest.java
@@ -1,22 +1,63 @@
 package it.euris.ires;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
 public class CalculatorTest {
-    Calculator calculator = new Calculator();
+    Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
     @Test
     @DisplayName("Test somma di 2 numeri")
+    void addTwoNumbers() {
 
-    void addTwoNumbers(){
-
-        assertEquals(5,calculator.add(2,3) );
+        assertEquals(5, calculator.add(2, 3));
     }
 
     @Test
     @DisplayName("Test sottrazione 2 numeri")
-    void subTwoNumbers(){
-        assertEquals(5,calculator.sub(10,5));
+    void subTwoNumbers() {
+        assertEquals(5, calculator.sub(10, 5));
+    }
+
+    @Test
+    @DisplayName("GIVEN Dati dei valori in input interi a, b WHEN tramite metodo multiplication() THEN ottengo risultato della moltiplicazione a*b")
+    void multiplyTwoNumbers() {
+        //arrange
+        int numberA = 10;
+        int numberB = 5;
+        int expected_result = 50;
+        Calculator calculator = new Calculator();
+
+        //act
+        int result = calculator.multiplication(numberA, numberB);
+
+        //assert
+        assertEquals(expected_result, result, String.format("%s * %s should be %s", numberA, numberB, expected_result));
+
+    }
+
+    @Test
+    @DisplayName("GIVEN Dati dei valori in input interi a, b con dividendo diverso da 0 WHEN tramite metodo division() THEN ottengo risultato della divisione a/b")
+    void divideTwoNumbers() throws Exception {
+        //arrange
+        int numberA = 10;
+        int numberB = 5;
+        int expected_result = 2;
+        Calculator calculator = new Calculator();
+
+        //act
+        int result = calculator.division(numberA, numberB);
+
+        //assert
+        assertEquals(expected_result, result, String.format("%s / %s should be %s", numberA, numberB, expected_result));
+
     }
 }
